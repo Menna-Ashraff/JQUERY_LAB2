0c75e26abc059739b6e162422568a4de9806beba
menna ashraf <you@example.com>
2024-02-15 21:37:00 +0200

    jquery

---
+++ product.js
@@ -0,0 +1,30 @@
+$(function() 
+{
+    const urlParams = new URLSearchParams(window.location.search)
+    const productId = urlParams.get('id')
+
+    $.ajax({
+        url: `https://dummyjson.com/products/${productId}`,
+        success: function (res) {
+            if (res.id) 
+            {
+                displayProductDetails(res)
+            }
+        },
+        error: function (err) {
+            console.log(err)
+        },
+        data: {},
+    })
+})
+
+function displayProductDetails(product, isFirstProduct = false) 
+{
+    const productDetailsContainer = $("<div>").addClass("product-details")
+    const img = $("<img>").attr("src", product.images[0]).addClass("product-img")
+    const title = $("<div>").text(product.title).addClass("product-title")
+    const desc = $("<div>").text(product.description).addClass("product-desc")
+    const price = $("<div>").text(product.price).addClass("product-price")
+    productDetailsContainer.append(img, title, desc, price)
+    productDetailsContainer.hide().appendTo("#product-details").fadeIn(1000)
+}
